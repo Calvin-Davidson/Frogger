0bffdda7fafa515e4f13f06a18604b1548011e14
EmileDavidson <Emiledavidson395@gmail.com>
2020-04-22 16:03:07 +0200

    Auto Map generating Toegevoegd

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
     public GameObject[] saveZones;
     public GameObject[] roads;
-    public GameObject[] Generated;
+    public GameObject LastPlaced;
 
     public void CreateNewMap()
     {
+        int Random = UnityEngine.Random.Range(0, 100);
+        if (Random <= 20)
+        {
+            Random = UnityEngine.Random.Range(0, saveZones.Length);
+            GameObject obj = Instantiate(saveZones[Random]);
+            obj.transform.position = LastPlaced.transform.position + new Vector3(0,0, 5);
+            LastPlaced = obj;
+        }
+        else
+        {
+            Random = UnityEngine.Random.Range(0, roads.Length);
+            GameObject obj = Instantiate(roads[Random]);
+            obj.transform.position = LastPlaced.transform.position + new Vector3(0,0, 5);
+            LastPlaced = obj;
+        }
     }
 }
--- Assets/Scripts/Vehicles/VehicleMovement.cs
+++ Assets/Scripts/Vehicles/VehicleMovement.cs
@@ -17,11 +17,19 @@ public class VehicleMovement : MonoBehaviour
     {
         if (Direction == direction.left)
         {
+            if ((transform.position.x >= 100 || transform.position.x <= -100))
+            {
+                Destroy(this.gameObject);
+            }
             transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);            
         }
 
         if (Direction == direction.right)
         {
+            if ((transform.position.x >= 100 || transform.position.x <= -100))
+            {
+                Destroy(this.gameObject);
+            }
             transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0);
         }
     }
